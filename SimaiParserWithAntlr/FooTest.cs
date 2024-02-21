790c6d228987a3ad10b3d62f35bff43ddd4386d9
Moying-moe <741744469@qq.com>
2024-02-22 00:41:31 +0800

    调整项目结构，添加单元测试项目

---
+++ SimaiParserWithAntlr/FooTest.cs
@@ -0,0 +1,25 @@
+﻿namespace SimaiParserWithAntlr
+{
+    public class FooTest
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+
+        public FooTest()
+        {
+            A = 0;
+            B = 0;
+        }
+
+        public FooTest(int a, int b)
+        {
+            A = a;
+            B = b;
+        }
+
+        public int Foo()
+        {
+            return A + B;
+        }
+    }
+}
